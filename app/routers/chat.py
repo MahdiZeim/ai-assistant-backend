0c7e53a948a0612cdc8e7c3c619d6a9a001bce79
Mahdi Amiri <amiri.mahdi2013@yahoo.com>
2026-07-13 14:30:07 +0330

    Refactor API using FastAPI routers

---
+++ app/routers/chat.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.services.memory_service import (
+    add_message,
+    get_history,
+)
+from app.services.ollama_service import ask_llm
+
+
+router = APIRouter(
+    prefix="/chat",
+    tags=["Chat"]
+)
+
+
+class ChatRequest(BaseModel):
+    session_id: str
+    message: str
+
+
+@router.post("/")
+def chat(
+    req: ChatRequest,
+    db: Session = Depends(get_db),
+):
+
+    print("message sent, waiting for response")
+    add_message(
+        db=db,
+        session_id=req.session_id,
+        role="user",
+        content=req.message,
+    )
+
+    history = get_history(
+        db=db,
+        session_id=req.session_id,
+    )
+
+    answer = ask_llm(history)
+
+    add_message(
+        db=db,
+        session_id=req.session_id,
+        role="assistant",
+        content=answer,
+    )
+
+    return {
+        "user_message": req.message,
+        "ai_response": answer,
+    }
--- main.py
+++ main.py
@@ -1,61 +1,20 @@
 from fastapi import FastAPI
-from fastapi import Depends
-
-from pydantic import BaseModel
-
-from sqlalchemy.orm import Session
-
-from app.services.ollama_service import ask_llm
-from app.services.memory_service import (
-    add_message,
-    get_history,
-)
 
 from app.database.database import Base, engine
-from app.database.database import get_db
 from app.models.message import Message
+from app.routers.chat import router as chat_router
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-class ChatRequest(BaseModel):
-    session_id: str
-    message: str
+
+app.include_router(chat_router)
+
 
 @app.get("/")
 def home():
-    return {"status": "AI Assistant is running 🚀"}
-
-
-@app.post("/chat")
-def chat(
-    req: ChatRequest,
-    db: Session = Depends(get_db),
-):
-    print("message sent. waiting for response")
-    add_message(
-        db=db,
-        session_id=req.session_id,
-        role="user",
-        content=req.message,
-    )
-
-    history = get_history(
-        db=db,
-        session_id=req.session_id,
-    )
-
-    answer = ask_llm(history)
-
-    add_message(
-        db=db,
-        session_id=req.session_id,
-        role="assistant",
-        content=answer,
-    )
-
     return {
-        "user_message": req.message,
-        "ai_response": answer,
+        "status": "AI Assistant is running 🚀"
     }
